4fa0c3f0f8eb5c6cdd31d65a3234647f1c9ee0fb
soph-havell <SHavell@academytrainee.com>
2021-04-16 11:03:55 +0100

    final changes

--- subnets/main.tf
+++ subnets/main.tf
@@ -8,6 +8,16 @@ resource "aws_subnet" "public" {
     }
 }
 
+resource "aws_subnet" "private" {
+    vpc_id = var.vpc_id
+    cidr_block = "10.0.2.0/24"
+    availability_zone = "eu-west-1b"
+
+    tags = {
+        Name = "production" 
+    }
+}
+
 resource "aws_route_table_association" "a" {
     subnet_id = aws_subnet.public.id 
     route_table_id = var.route_id 
@@ -21,7 +31,33 @@ resource "aws_network_interface" "i" {
 
 resource "aws_eip" "one" {
   vpc = true
-  network_interface = aws_network_interface.i.id
-  associate_with_private_ip = "10.0.1.50"
   depends_on = [var.internet_gate]
+}
+
+resource "aws_nat_gateway" "gw" {
+    depends_on = [aws_eip.one]
+    allocation_id = aws_eip.one.id 
+    subnet_id = aws_subnet.public.id 
+
+    tags = {
+        Name = "gw NAT"
+    }
+}
+
+resource "aws_route_table" "NAT_gateway_RT" {
+    depends_on = [aws_nat_gateway.gw]
+    vpc_id = var.vpc_id 
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.gw.id
+    }
+
+    tags = {
+        Name = "Route table for Gateway"
+    }   
+}
+
+resource "aws_route_table_association" "b" {
+    subnet_id = aws_subnet.private.id 
+    route_table_id = aws_route_table.NAT_gateway_RT.id
 }
